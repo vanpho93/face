e6d3d06cf1e3b429bc94ae980588f5ecb2e8a182
Admin <vanpho02@gmail.com>
2017-03-14 00:42:12 +0700

    lan cuoi

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const parser = require('body-parser').urlencoded({ extended: false });
-// const formidble = require('express-formidable');
+const formidble = require('express-formidable')({ uploadDir: './public' });
+const fs = require('fs');
+
 const getFaceByImageUrl = require('./api/uploadFace');
 const getNameByFaceId = require('./api/identify');
 
 const app = express();
-app.listen(3000, () => console.log('server started'));
+app.use(express.static('public'));
+app.listen(process.env.PORT || 3000, () => console.log('server started'));
 
-app.post('/find', parser, (req, res) => {
+app.post('/findById', parser, (req, res) => {
     const { imageUrl } = req.body;
     try {
         const faceId = getFaceByImageUrl(imageUrl);
@@ -16,4 +19,15 @@ app.post('/find', parser, (req, res) => {
     } catch (e) {
         res.send(`${e} `);
     }
+});
+
+app.post('/findByImage', formidble, (req, res) => {
+    let { path } = req.files.avatar;
+    fs.rename(path, `${path}.jpg`, err => {
+        if (err) return res.send(`${err} `);
+        path = `${path}.jpg`;
+        const output = path.replace('public/', '');
+        console.log(output);
+        res.send(output);
+    });
 });
